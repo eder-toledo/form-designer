6885eec99b1bf7137b8c6f8c05e36cf1fc7da74e
Eder Valente Toledo Nuñez <edertoledon@gmail.com>
2017-01-13 23:57:02 -0600

    Se agrego parte del menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-jQuery(document).ready(function(){
-    
+$(document).ready(function () {
+    var menuelements = null;
+    var i = 0;
+    for (i; i < menuelements.length; i++) {
+        var element = '<center class="menu-element">' + menuelements[i].icon + '' + menuelements[i].name + '</center>';
+        $(".menu").append(element);
+    }
 });
