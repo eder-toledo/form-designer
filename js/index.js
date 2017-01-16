b5a7cc507fcfb7b3f27b3a5566072eaf45e61223
Eder Valente Toledo Nuñez <edertoledon@gmail.com>
2017-01-16 09:22:12 -0600

    Se integro el json

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
-    var menuelements = null;
-    var i = 0;
-    for (i; i < menuelements.length; i++) {
-        var element = '<center class="menu-element">' + menuelements[i].icon + '' + menuelements[i].name + '</center>';
-        $(".menu").append(element);
-    }
+    $.getJSON('js/menu-element.json', function(data) {
+        var menuelements = data;
+        var i = 0;
+        for (i; i < menuelements.length; i++) {
+            var element = '<center class="menu-element">' + menuelements[i].icon + '' + menuelements[i].name + '</center>';
+            $(".menu").append(element);
+        }
+    });
 });
